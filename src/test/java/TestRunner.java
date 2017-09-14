512643e498c0decc0f10ed5fa9a9860971400065
Oleg <luckydev3000@gmail.com>
2017-09-14 12:51:36 -0500

    added screen of passed tests

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,10 +4,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"src/test/resources/screens"}
-        , features = "src/test/resources/features"
-        , glue = "steps"
-)
+        features = "src/test/resources/features"
+        , glue = "steps")
 
 public class TestRunner {
 }
